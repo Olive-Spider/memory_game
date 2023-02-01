4a05536a2235420a9af61fe7981c68834e55910f
Livya Kendi Kiruja <livyakendikiruja@gmail.com>
2023-02-01 23:53:32 +0300

    on click functionalities added

---
+++ src/app.js
@@ -0,0 +1,88 @@
+const cardArray = [
+    {
+        name: 'fries',
+        img: '/images/fries.png',
+    },
+
+    {
+        name: 'pizza',
+        img: '/images/pizza.png',
+    },
+
+    {
+        name: 'hotdog',
+        img: '/images/hotdog.png',
+    },
+
+    {
+        name: 'milkshake',
+        img: '/images/milkshake.png',
+    },
+
+    {
+        name: 'cheeseburger',
+        img: '/images/cheeseburger.png',
+    },
+
+    {
+        name: 'ice-cream',
+        img: '/images/ice-cream.png',
+    },
+
+    {
+        name: 'fries',
+        img: '/images/fries.png',
+    },
+
+    {
+        name: 'pizza',
+        img: '/images/pizza.png',
+    },
+
+    {
+        name: 'hotdog',
+        img: '/images/hotdog.png',
+    },
+
+    {
+        name: 'milkshake',
+        img: '/images/milkshake.png',
+    },
+
+    {
+        name: 'cheeseburger',
+        img: '/images/cheeseburger.png',
+    },
+
+    {
+        name: 'ice-cream',
+        img: '/images/ice-cream.png',
+    },
+]
+
+cardArray.sort(() => 0.5 - Math.random());
+
+const gridDisplay = document.querySelector('.grid')
+const cardsChosen = []
+
+function createBoard(){
+    for (let i = 0; i < cardArray.length; i++){
+        const card = document.createElement('img');
+        card.setAttribute('src', '/images/blank.png')
+        card.setAttribute('data-id', i)
+        card.addEventListener('click', flipCard)
+        console.log(card, i)
+        gridDisplay.appendChild(card)
+    }
+}
+createBoard();
+
+function flipCard(){
+    console.log(cardArray)
+    const cardId = this.getAttribute('data-id')
+    cardsChosen.push(cardArray[cardId].name)
+    console.log('I have been clicked', cardId)
+    console.log(cardsChosen)
+    this.setAttribute('src', cardArray[cardId].img)
+    
+}
